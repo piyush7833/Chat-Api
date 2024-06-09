ea1fd6acb1b7d85b63a6bfd5bb033b0a76822f66
Piyush Singh <ps671248@gmail.com>
2024-06-09 13:36:18 +0530

    workflow

--- test/user.go
+++ test/user.go
@@ -1,1 +1,16 @@
 package test
+
+import (
+	"log"
+)
+
+func init() {
+	err := Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// func TestGetAllUser(t *testing.T) {
+// 	TestGetRequest(t, "/api/protected/user/get-all?page=0", 200, "Users retrieved successfully")
+// }
